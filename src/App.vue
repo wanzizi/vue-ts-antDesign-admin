94fa0a3cb8006b9aa08db2337bdcd841b78393ea
yezhi <496574509@qq.com>
2019-07-08 16:50:15 +0800

    chore(ts):add ant-vue's declare file and modify lang to ch_ZN

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="app">
+  <a-locale-provider :locale="locale" id="app">
     <router-view/>
-  </div>
+  </a-locale-provider>
 </template>
+
+<script lang="ts">
+import { LocaleProvider } from 'ant-design-vue'
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    'a-locale-provider': LocaleProvider
+  }
+})
+export default class App extends Vue {
+  locale:any = zhCN
+}
+</script>
 
 <style lang="less">
 *{
