de4a2a96c4e9d2465c9d2798c1d1f26c2dda9cf8
yezhi <496574509@qq.com>
2019-07-09 18:18:40 +0800

    feat(form/baseForm):dev base form

---
+++ src/interface/index.ts
@@ -0,0 +1,10 @@
+// 通用或常用接口
+
+// 表单使用的栅格对象
+interface spanObj{
+    span:number
+}
+export interface colObj{
+    xs:spanObj,
+    sm:spanObj,
+}
--- src/router/form.ts
+++ src/router/form.ts
@@ -14,7 +14,17 @@ export default {
         chineseName: '基础表单',
         desc: '表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。'
       },
-      component: () => import('@/views/form/FormBase.vue')
+      component: () => import('@/views/form/FormBase')
+    },
+    {
+      path: 'form-step',
+      name: 'form-step',
+      meta: {
+        chineseName: '分步表单',
+        desc: '将一个冗长或用户不熟悉的表单任务分成多个步骤，指导用户完成。'
+      },
+      component: () => import('@/views/form/FormStep.vue')
+      // (resolve: any) => require(['@/views/form/FormBase'], resolve)
     }
   ]
 }
